e8bd9d9da91ccb2290acd991bf9b47bcbca468d9
rustam <rustam2105@yandex.ru>
2025-05-21 10:00:13 +0500

    PEP8 linting

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['84.201.163.115', '127.0.0.1', 'localhost','kittygramru.zapto.org']
+ALLOWED_HOSTS = ['84.201.163.115', '127.0.0.1', 'localhost', 'kittygramru.zapto.org']
 
 
 # Application definition
