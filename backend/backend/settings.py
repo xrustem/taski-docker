96154afdc7ec937d00ed266d55cb8de0e0160fa3
rustam <rustam2105@yandex.ru>
2025-05-21 13:45:34 +0500

    image

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['84.201.163.115', '127.0.0.1', 'localhost', 'kittygramru.zapto.org']
+ALLOWED_HOSTS = ['84.201.163.115', '127.0.0.1', 'localhost', 'rutaski.zapto.org']
 
 
 # Application definition
